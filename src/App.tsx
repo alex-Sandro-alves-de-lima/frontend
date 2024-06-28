9ee1d6187d19852b1735abc8af3154b947a283a8
alex-Sandro-alves-de-lima <lexjustin5@gmail.com>
2024-06-28 12:10:34 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,20 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { UserOutlined, VideoCameraOutlined, LoginOutlined } from '@ant-design/icons';
-import { Layout, Menu, theme, MenuProps, Avatar, Tooltip, Modal, ConfigProvider, ColorPicker } from 'antd';
+import { Layout, Menu, theme, MenuProps, Avatar, Tooltip, Modal } from 'antd';
 import { useAuthStore } from './store/authStore';
-import type { ColorPickerProps } from 'antd';
 
 const { Header, Content, Footer, Sider } = Layout;
-
-type GetProp<T, K extends keyof T> = T[K];
-type Color = GetProp<ColorPickerProps, 'value'>;
 
 const App: React.FC = () => {
     const { logout } = useAuthStore();
     const navigate = useNavigate();
     const [isModalVisible, setIsModalVisible] = useState(false);
 
-    const [colorHex, setColorHex] = useState<Color>('#DBE2EB');
-
-    const hexString = useMemo<string>(() => (
-        (typeof colorHex === 'string' ? colorHex : colorHex?.toHexString() ?? '#DBE2EB')
-    ), [colorHex]);
-
     type MenuItem = Required<MenuProps>['items'][number];
+
     const {
-        token: { colorBgContainer },
+        token: { colorBgContainer }
     } = theme.useToken();
 
     function getItem(
@@ -62,80 +53,60 @@ const App: React.FC = () => {
     };
 
     return (
-        <ConfigProvider
-            theme={{
-                token: {
-                    colorPrimary: `${hexString}`,
-                    borderRadius: 6,
-                    colorBgContainer: '#C4D3E7',
-                },
-                components: {
-                    Descriptions: {
-                        colorSplit: "#C4D3E7",
-                        colorFillAlter: '#C4D3E7',
-                        colorText: '#040A13'
-                    },
-                    Card: {
-                        colorFillAlter: "rgb(244, 244, 244)",
-                        colorBgContainer: "#FFF"
-                    }
-                }
-            }}
-        >
-            <Layout style={{ height: '100vh' }}>
-                <Sider
-                    breakpoint="lg"
-                    collapsedWidth="0"
-                    onBreakpoint={(broken) => {
-                        console.log(broken);
+        <Layout style={{ minHeight: '100vh' }}>
+            <Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                onBreakpoint={(broken) => {
+                    console.log(broken);
+                }}
+                onCollapse={(collapsed, type) => {
+                    console.log(collapsed, type);
+                }}
+               // style={{ background: colorBgContainer }}
+            >
+                <div className="demo-logo-vertical">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/9/9f/Babygrande_300x120_Logo.jpg" alt="Logo" style={{ width: '100%', padding: '16px' }} />
+                </div>
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['login']}
+                    onClick={(event) => {
+                        navigate(event.key);
                     }}
-                    onCollapse={(collapsed, type) => {
-                        console.log(collapsed, type);
-                    }}
-                >
-                    <div className="demo-logo-vertical">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/9/9f/Babygrande_300x120_Logo.jpg" alt="Logo" style={{ width: '100%', padding: '16px' }} />
+                  //  style={{ background: colorBgContainer }}
+                    items={items}
+                />
+            </Sider>
+            <Layout>
+                <Header style={{ 
+                    //background: colorBgContainer,
+                     padding: '0 16px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <div>asd</div>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <Tooltip title={<div><div>Nome: Alex Sandro</div><div>Nível de Acesso: Admin</div></div>}>
+                            <div style={{ padding: 4, display: 'flex', alignItems: 'center', cursor: 'pointer' }}>
+                                <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />
+                                <div style={{ marginLeft: 8 }}>Alex S</div>
+                            </div>
+                        </Tooltip>
+                        <LoginOutlined style={{ fontSize: '20px', marginLeft: 16, cursor: 'pointer' }} onClick={showLogoutModal} />
                     </div>
-                    <Menu
-                        theme="dark"
-                        mode="inline"
-                        defaultSelectedKeys={['login']}
-                        onClick={(event) => {
-                            navigate(event.key);
-                        }}
-                        items={items}
-                    />
-                </Sider>
-                <Layout>
-                    <Header style={{ padding: 0, background: colorBgContainer, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} >
-                        <div style={{ paddingLeft: '16px' }}>asd</div>
-                        <div style={{ display: 'flex', alignItems: 'center', paddingRight: '16px' }}>
-                            <Tooltip title={<div><div>Nome: Alex Sandro</div><div>Nível de Acesso: Admin</div></div>}>
-                                <div style={{ padding: 4, display: 'flex', alignItems: 'center', cursor: 'pointer' }}>
-                                    <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />
-                                    <div style={{ marginLeft: 8 }}>Alex S</div>
-                                </div>
-                            </Tooltip>
-                            <LoginOutlined style={{ fontSize: '20px', marginLeft: 16, cursor: 'pointer' }} onClick={showLogoutModal} />
-                        </div>
-                    </Header>
-                    <Content style={{ margin: '24px 16px 0', overflow: 'auto' }}>
-                        <div style={{ padding: 24, minHeight: 'calc(100vh - 112px)', background: colorBgContainer }}>
-                            <ColorPicker
-                                value={colorHex}
-                                onChange={setColorHex}
-                                format="hex"
-                            />
-                            <Outlet />
-                        </div>
-                    </Content>
-                    <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
-                </Layout>
+                </Header>
+                <Content style={{ margin: '24px 16px 0', overflow: 'auto'
+                   // , background: colorBgContainer 
+                    }}>
+                    <div style={{ padding: 24, minHeight: 'calc(100vh - 112px)' }}>
+                        <Outlet />
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center', background: colorBgContainer }}>Ant Design ©2023 Created by Ant UED</Footer>
                 <Modal title="Logout" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} okText="Sim" cancelText="Não">
                     <p>Você deseja deslogar da aplicação?</p>
                 </Modal>
             </Layout>
-        </ConfigProvider>
+        </Layout>
     );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { RouterProvider } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { ConfigProvider } from 'antd'; // Remova Menu e Sider se não forem usados
 import router from './routes/routes';
+
 
 const clientId = "117403387590-rr88piui2d41mnagjonfn4a1lip0ptt4.apps.googleusercontent.com";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+ReactDOM.render(
   <GoogleOAuthProvider clientId={clientId}>
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: 'red', // Cor primária do tema
+            borderRadius: 7,
+            //colorBgContainer: "black", // Cor de fundo padrão
+            //colorText:"red"
+          },
+          components: {
+            Descriptions: {
+              colorSplit: "#C4D3E7",
+              colorFillAlter: '#C4D3E7',
+              colorText: '#040A13'
+            },
+            Card: {
+              colorFillAlter: "rgb(244, 244, 244)",
+              colorBgContainer: "#FFF"
+            },
+            Layout: {
+              colorBgContainer: 'red', // Cor de fundo do Sider
+              colorText: '#FFFFFF', // Cor do texto dentro do Sider
+            }
+            
+          }
+        }}
+      >
+        <RouterProvider router={router} />
+      </ConfigProvider>
     </React.StrictMode>
-  </GoogleOAuthProvider>
+  </GoogleOAuthProvider>,
+  document.getElementById('root')
 );
